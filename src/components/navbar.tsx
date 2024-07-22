dc7ad0392321a0bd50f8241ac802d75824a43473
Ianroery <ianroeri@gmail.com>
2024-07-22 08:24:28 +0700

    solve confiq

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,11 +18,10 @@ const Navbar = () => {
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 py-12">
         <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-          <img src="/public/photo.png" alt="" width={90} height={90} />
-          <span className="sr-only">Library App</span>
+          <h1 className="sr-only gap-2 font-bold font-Poppins text-4xl ">Soap Shop</h1>
         </Link>
         <Link to="/books" className="text-muted-foreground transition-colors hover:text-foreground">
-          Library
+          <h1 className="font-bold font-Poppins text-3xl "> Soap</h1>
         </Link>
       </nav>
       <Sheet>
@@ -39,7 +38,7 @@ const Navbar = () => {
               <span className="sr-only">Library App</span>
             </Link>
             <Link to="/" className="text-muted-foreground hover:text-foreground">
-              Library
+              <h1 className="font-bold font-Poppins text-3xl "> Soap</h1>
             </Link>
           </nav>
         </SheetContent>
